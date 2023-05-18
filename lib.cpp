f64a7e262d1c33dca367a47e98c10cfc9113187e
fabriziorosichini <127482458+fabriziorosichini@users.noreply.github.com>
2023-05-18 16:01:58 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include "lib.h"
 
+bool verifica (char n){
+    if ((n>='a' and n<='z')or (n>='A' and n<='Z')){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+char conversione (char n){
+    char c;
+    if (n>='a' && n<='z'){
+        c=n-32;
+        return c;
+    }else if (n>='A' && n<='Z'){
+        c=n+32;
+        return c;
+    }
+    return 0;
+}
