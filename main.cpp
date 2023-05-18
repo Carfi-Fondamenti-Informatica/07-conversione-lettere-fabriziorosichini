30a59e459f7c6f3840e7a628f521e31ee78acc2f
fabriziorosichini <127482458+fabriziorosichini@users.noreply.github.com>
2023-05-18 16:02:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char c_input;
+    cin>>c_input;
+   bool carattere=verifica(c_input);
+    if (carattere ) {
+        cout <<  conversione (c_input)<< endl;
+    }else{
+        cout<<"errore";
+    }
+
+    return 0;
 }
